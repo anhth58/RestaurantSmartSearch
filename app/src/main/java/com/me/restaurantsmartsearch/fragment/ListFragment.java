45b051b42ce8b9af37d709db60f1ddaaad712c0c
Tran Hoang Anh <Tran Hoang Anh>
2017-05-12 10:31:16 +0700

    updated

--- app/src/main/java/com/me/restaurantsmartsearch/fragment/ListFragment.java
+++ app/src/main/java/com/me/restaurantsmartsearch/fragment/ListFragment.java
@@ -5,12 +5,15 @@ import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.Rect;
 import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.util.TypedValue;
@@ -34,7 +37,11 @@ import com.me.restaurantsmartsearch.adapter.RestaurantRecommendAdapter;
 import com.me.restaurantsmartsearch.adapter.SuggestAdapter;
 import com.me.restaurantsmartsearch.async.SearchAsyncTask;
 import com.me.restaurantsmartsearch.async.SuggestAsyncTask;
+import com.me.restaurantsmartsearch.controller.PinController;
+import com.me.restaurantsmartsearch.model.Pin;
 import com.me.restaurantsmartsearch.model.Restaurant;
+import com.me.restaurantsmartsearch.nlp.ContextNLP;
+import com.me.restaurantsmartsearch.nlp.RestaurantNLP;
 import com.me.restaurantsmartsearch.utils.AccentRemover;
 import com.me.restaurantsmartsearch.utils.Constant;
 import com.me.restaurantsmartsearch.utils.Utils;
@@ -45,6 +52,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
 
 import io.realm.Realm;
 
@@ -53,11 +62,12 @@ import io.realm.Realm;
  */
 public class ListFragment extends BaseFragment {
     ArrayList<Restaurant> listResult = new ArrayList<>();
-    RestaurantRecommendAdapter restaurantAdapter;
+    ArrayList<Restaurant> listRecommended = new ArrayList<>();
+    RestaurantRecommendAdapter restaurantAdapter, recommendedAdapter;
     Realm realmUI;
-    private RecyclerView recyclerView;
-
-
+    private RecyclerView recyclerView, recommendedRecycle;
+    private PinController pinController;
+    private Realm realmSearch;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -66,11 +76,39 @@ public class ListFragment extends BaseFragment {
         initView(view);
         initListener();
         initData();
+        pinController = PinController.with(getActivity());
+        Calendar rightNow = Calendar.getInstance();
+        final int hour1 = rightNow.get(Calendar.HOUR_OF_DAY);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(hour1 < 11){
+                    searchOnline("Coffee gần đây",0);
+                }
+                else if(hour1 >= 11 && hour1 <13){
+                    searchOnline("Cơm giá rẻ gần đây",0);
+                }
+                else if(hour1 >= 13 && hour1 <= 17){
+                    searchOnline("Trà sữa gần đây",0);
+                }
+                else if(hour1 == 17){
+                    searchOnline("Bia hơi gần đây",0);
+                }
+                else if(hour1 >= 18 && hour1 <20){
+                    searchOnline("Cơm văn phòng gần đây", 0);
+                }
+                else if (hour1 >= 20){
+                    searchOnline("Cà phê gần đây",0);
+                }
+            }
+        },2000);
+
         return view;
     }
 
     public void initView(View view) {
         recyclerView = (RecyclerView) view.findViewById(R.id.lv_restaurant);
+        recommendedRecycle = (RecyclerView) view.findViewById(R.id.lvRecommended);
     }
 
     public void initListener() {
@@ -82,16 +120,15 @@ public class ListFragment extends BaseFragment {
         realmUI = Realm.getDefaultInstance();
         Log.d("Size",listResult.size() +"");
         if (!realmUI.isEmpty() && listResult.size() == 0) {
-            listResult = new ArrayList<>(realmUI.where(Restaurant.class).greaterThan("id",25505).lessThan("id",25520).findAll());
+            listResult = new ArrayList<>(realmUI.where(Restaurant.class).greaterThan("id",25505).lessThan("id",25510).findAll());
             restaurantAdapter = new RestaurantRecommendAdapter(getActivity(), listResult);
             RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getActivity(), 1);
             recyclerView.setLayoutManager(mLayoutManager);
             recyclerView.addItemDecoration(new GridSpacingItemDecoration(1, dpToPx(10), true));
             recyclerView.setItemAnimator(new DefaultItemAnimator());
             recyclerView.setAdapter(restaurantAdapter);
-
         } else if(listResult.size() >0 && listResult.get(0).isValid()){
-//            restaurantAdapter = new RestaurantAdapter(getActivity(), listResult);
+//          restaurantAdapter = new RestaurantAdapter(getActivity(), listResult);
             recyclerView.setAdapter(restaurantAdapter);
             restaurantAdapter.notifyDataSetChanged();
         }
@@ -100,6 +137,78 @@ public class ListFragment extends BaseFragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    public void searchOnline(String s, final int pageResult) {
+        //debug purpose, remove later
+        //example nlp
+        ContextNLP result = RestaurantNLP.query(s);
+        HashMap<String, String> hashMap = result.getHashMap();
+        /*
+        * Right now just three types are available:
+        * ContextNLP.TYPE_CHEAP, ContextNLP.TYPE_NEAR, ContextNLP.TYPE_LOCATION
+        * */
+        String type, address, name, nearLocation;
+
+        type = hashMap.get(ContextNLP.FIELD_TYPE);
+        address = hashMap.get(ContextNLP.FIELD_ADDRESS);
+        name = hashMap.get(ContextNLP.FIELD_NAME);
+        nearLocation = hashMap.get(ContextNLP.FIELD_NEAR_LOCATION);
+
+        Log.d("Hasmap", hashMap.toString());
+
+        hideSoftKeyboard();
+        String fields[] = {"name", "", ""};
+        if (!TextUtils.isEmpty(address)) {
+            fields[1] = "address";
+            s = name.trim() + " " + address.trim();
+        }
+
+        Pin location = null;
+        nearLocation = AccentRemover.removeAccent(nearLocation.trim());
+        if (!TextUtils.isEmpty(nearLocation)) {
+            if (!nearLocation.equals(Constant.HERE) && !nearLocation.equals(Constant.DAY)){
+                location = pinController.getPinByLocation(nearLocation);
+            }
+            else {
+                location = new Pin();
+                location.setLongitude(Utils.currentLong);
+                location.setLatitude(Utils.currentLat);
+                Log.d("location", Utils.currentLat +" "+ Utils.currentLong);
+            }
+            s = name;
+        }
+        if (location == null) {
+            location = new Pin();
+        }
+
+        SearchAsyncTask searchAsyncTask = new SearchAsyncTask(AccentRemover.removeAccent(s),0, pageResult, type, location.getLongitude(), location.getLatitude(), fields, new SearchAsyncTask.OnSearchComplete() {
+            @Override
+            public void onSearchComplete(String response) {
+                try {
+                    Realm.init(getActivity());
+                    realmSearch = Realm.getDefaultInstance();
+                    JSONObject jsonObject = new JSONObject(response);
+                    JSONObject jsonObject1 = jsonObject.getJSONObject(Constant.HITS);
+                    JSONArray jsonArray = jsonObject1.getJSONArray(Constant.HITS);
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        int k = jsonArray.getJSONObject(i).optInt(Constant._ID);
+                        listRecommended.add(realmSearch.where(Restaurant.class).equalTo(Constant.ID, k).findFirst());
+                    }
+                    if (listRecommended.size() > 0) {
+                        recommendedAdapter = new RestaurantRecommendAdapter(getActivity(), listRecommended);
+                        RecyclerView.LayoutManager mLayoutManager2 = new GridLayoutManager(getActivity(), 1);
+                        recommendedRecycle.setLayoutManager(mLayoutManager2);
+                        recommendedRecycle.addItemDecoration(new GridSpacingItemDecoration(1, dpToPx(10), true));
+                        recommendedRecycle.setItemAnimator(new DefaultItemAnimator());
+                        recommendedRecycle.setAdapter(recommendedAdapter);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        searchAsyncTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 
     @Override
@@ -115,13 +224,21 @@ public class ListFragment extends BaseFragment {
             Log.d("AA","invalid");
             Realm.init(getActivity());
             realmUI = Realm.getDefaultInstance();
-            listResult = new ArrayList<>(realmUI.where(Restaurant.class).greaterThan("id",25505).lessThan("id",25520).findAll());
+            listResult = new ArrayList<>(realmUI.where(Restaurant.class).greaterThan("id",25505).lessThan("id",25510).findAll());
             restaurantAdapter = new RestaurantRecommendAdapter(getActivity(), listResult);
             RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getActivity(), 1);
             recyclerView.setLayoutManager(mLayoutManager);
             recyclerView.addItemDecoration(new GridSpacingItemDecoration(1, dpToPx(10), true));
             recyclerView.setItemAnimator(new DefaultItemAnimator());
             recyclerView.setAdapter(restaurantAdapter);
+
+//            listRecommended = new ArrayList<>(realmUI.where(Restaurant.class).greaterThan("id",25505).lessThan("id",25520).findAll());
+//            recommendedAdapter = new RestaurantRecommendAdapter(getActivity(), listRecommended);
+//            RecyclerView.LayoutManager mLayoutManager2 = new GridLayoutManager(getActivity(), 1);
+//            recommendedRecycle.setLayoutManager(mLayoutManager);
+//            recommendedRecycle.addItemDecoration(new GridSpacingItemDecoration(1, dpToPx(10), true));
+//            recommendedRecycle.setItemAnimator(new DefaultItemAnimator());
+//            recommendedRecycle.setAdapter(restaurantAdapter);
         }
         else {
             Log.d("AA","valid");
